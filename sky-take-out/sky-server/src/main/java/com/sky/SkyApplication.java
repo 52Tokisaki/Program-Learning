3d39da1cfaf25f20e8785f90fc2a3af740da81c6
LeSama <1506536196@qq.com>
2026-08-21 00:41:34 +0800

    使用swagger生成接口文档

--- sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,10 +1,8 @@
 package com.sky;
 
-import com.sky.config.WebMvcConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
