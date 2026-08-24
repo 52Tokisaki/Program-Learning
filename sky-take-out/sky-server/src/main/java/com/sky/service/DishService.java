6fc73a2a201f6f9b5b29bde7bef2425b7b811ec7
LeSama <1506536196@qq.com>
2026-08-24 19:47:52 +0800

    菜品分页查询接口功能实现 & 新增操作添加公共字段填充注解

--- sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
-import org.springframework.stereotype.Service;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
     void save(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
