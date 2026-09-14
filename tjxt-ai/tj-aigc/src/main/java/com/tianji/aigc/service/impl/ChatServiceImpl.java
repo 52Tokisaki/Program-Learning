a431c467addc0bc262f15706defdd6c04106b9d0
LeSama <1506536196@qq.com>
2026-09-15 00:40:12 +0800

    使用Redis终止流式对话输出

--- tjxt-ai/tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
+++ tjxt-ai/tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
@@ -8,6 +8,7 @@ import com.tianji.aigc.service.ChatService;
 import com.tianji.aigc.vo.ChatEventVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -22,7 +23,10 @@ public class ChatServiceImpl implements ChatService {
 
     // 使用一个容器， 来存储会话和是否停止的映射关系
     // 1. 使用ConcurrentHashMap来存储会话和是否停止的映射关系，  2. 对于分布式环境， 可以使用Redis来存储会话和是否停止的映射关系
-    private final ConcurrentHashMap<String, Boolean> GENERATE_STATUS = new ConcurrentHashMap<>();
+//    private final ConcurrentHashMap<String, Boolean> GENERATE_STATUS = new ConcurrentHashMap<>();
+
+    private final StringRedisTemplate stringRedisTemplate;
+
     @Override
     public Flux<ChatEventVO> chat(ChatDTO chatDTO) {
         return chatClient
@@ -34,10 +38,14 @@ public class ChatServiceImpl implements ChatService {
                 .user(chatDTO.getQuestion())
                 .stream()
                 .chatResponse()
-                .doFirst(() -> GENERATE_STATUS.put(chatDTO.getSessionId(), true))
-                .doOnError(throwable -> GENERATE_STATUS.remove(chatDTO.getSessionId()))
-                .doOnComplete(() -> GENERATE_STATUS.remove(chatDTO.getSessionId()))
-                .takeWhile(response -> GENERATE_STATUS.getOrDefault(chatDTO.getSessionId(), false)) // 用于控制流的结束
+                .doFirst(() -> stringRedisTemplate.opsForValue().set(chatDTO.getSessionId(), "true"))
+                .doOnError(throwable -> stringRedisTemplate.opsForValue().set(chatDTO.getSessionId(), "false"))
+                .doOnComplete(() -> stringRedisTemplate.opsForValue().set(chatDTO.getSessionId(), "false"))
+                .takeWhile(response -> "true".equals(stringRedisTemplate.opsForValue().get(chatDTO.getSessionId())))
+//                .doFirst(() -> GENERATE_STATUS.put(chatDTO.getSessionId(), true))
+//                .doOnError(throwable -> GENERATE_STATUS.remove(chatDTO.getSessionId()))
+//                .doOnComplete(() -> GENERATE_STATUS.remove(chatDTO.getSessionId()))
+//                .takeWhile(response -> GENERATE_STATUS.getOrDefault(chatDTO.getSessionId(), false)) // 用于控制流的结束
                 .map(chatResponse -> {
                     String result = chatResponse.getResult().getOutput().getText();
                     return ChatEventVO.builder()
@@ -52,6 +60,7 @@ public class ChatServiceImpl implements ChatService {
 
     @Override
     public void stop(String sessionId) {
-        GENERATE_STATUS.put(sessionId, false); // 设置会话为停止状态
+//        GENERATE_STATUS.put(sessionId, false); // 设置会话为停止状态
+        stringRedisTemplate.opsForValue().set(sessionId, "false"); // 设置会话为停止状态
     }
 }
