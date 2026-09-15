c43bc12e9232a1dc378a3b796805873c758c2805
LeSama <1506536196@qq.com>
2026-09-16 00:42:52 +0800

    解决停止生成时不会自动保存当前大模型正在输出的内容问题

--- tjxt-ai/tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
+++ tjxt-ai/tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
@@ -9,6 +9,7 @@ import com.tianji.aigc.vo.ChatEventVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -21,6 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ChatServiceImpl implements ChatService {
     private final ChatClient chatClient;
     private final SystemPromptConfig systemPromptConfig;
+    private final ChatMemory chatMemory;
 
     // 使用一个容器， 来存储会话和是否停止的映射关系
     // 1. 使用ConcurrentHashMap来存储会话和是否停止的映射关系，  2. 对于分布式环境， 可以使用Redis来存储会话和是否停止的映射关系
@@ -30,6 +32,7 @@ public class ChatServiceImpl implements ChatService {
 
     @Override
     public Flux<ChatEventVO> chat(ChatDTO chatDTO) {
+        StringBuilder outputBuilder = new StringBuilder();
         String conversationId = ChatService.getConversationId(chatDTO.getSessionId());
         return chatClient
                 .prompt()
@@ -42,6 +45,7 @@ public class ChatServiceImpl implements ChatService {
                 .stream()
                 .chatResponse()
                 .doFirst(() -> stringRedisTemplate.opsForValue().set(chatDTO.getSessionId(), "true"))
+                .doOnCancel(() -> saveStopHistoryRecord(conversationId, outputBuilder.toString())) // 停止输出时，将当前输出保存到会话中
                 .doOnError(throwable -> stringRedisTemplate.opsForValue().set(chatDTO.getSessionId(), "false"))
                 .doOnComplete(() -> stringRedisTemplate.opsForValue().set(chatDTO.getSessionId(), "false"))
                 .takeWhile(response -> "true".equals(stringRedisTemplate.opsForValue().get(chatDTO.getSessionId())))
@@ -51,6 +55,7 @@ public class ChatServiceImpl implements ChatService {
 //                .takeWhile(response -> GENERATE_STATUS.getOrDefault(chatDTO.getSessionId(), false)) // 用于控制流的结束
                 .map(chatResponse -> {
                     String result = chatResponse.getResult().getOutput().getText();
+                    outputBuilder.append(result); // 将结果追加到输出构建器中
                     return ChatEventVO.builder()
                             .eventData(result)
                             .eventType(ChatEventTypeEnum.DATA.getValue())
@@ -65,5 +70,15 @@ public class ChatServiceImpl implements ChatService {
     public void stop(String sessionId) {
 //        GENERATE_STATUS.put(sessionId, false); // 设置会话为停止状态
         stringRedisTemplate.opsForValue().set(sessionId, "false"); // 设置会话为停止状态
+    }
+
+    /**
+     * 保存停止输出的记录
+     *
+     * @param conversationId 会话id
+     * @param content        大模型输出的内容
+     */
+    private void saveStopHistoryRecord(String conversationId, String content) {
+        chatMemory.add(conversationId, new AssistantMessage(content));
     }
 }
